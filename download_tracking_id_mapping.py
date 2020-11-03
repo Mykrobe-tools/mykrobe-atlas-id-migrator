384095d63ac8c829a4900cb9fc9c6ebc5c6383dc
Giang Nguyen <giang@ebi.ac.uk>
2020-11-03 15:44:40 +0000

    Add download script

---
+++ download_tracking_id_mapping.py
@@ -0,0 +1,38 @@
+import argparse
+import pickle
+
+import tracking_client
+from tracking_client import Configuration, SampleApi
+
+
+def download(isolate_id_list_filepath, tracking_api_host_port):
+    [host, port] = tracking_api_host_port.split(':')
+
+    configuration = Configuration()
+    configuration.host = f"http://{host}:{port}/api/v1"
+    api_client = tracking_client.ApiClient(configuration=configuration)
+
+    sample_api = SampleApi(api_client)
+
+    mapping = {}
+
+    with open(isolate_id_list_filepath, 'r') as inf:
+        for iid in inf:
+            from_tracking_api = sample_api.samples_get(isolate_id=iid)
+
+            if not from_tracking_api:
+                mapping[iid] = iid
+                continue
+
+            mapping[iid] = from_tracking_api[0].id
+
+    print(pickle.dumps(mapping))
+
+
+parser = argparse.ArgumentParser(description='Download ID mapping from Tracking API.')
+parser.add_argument('isolate_id_list_filepath', type=str, help='Path to input file. Expects each line is an isolate ID')
+parser.add_argument('tracking_api_host_port', type=str, help='Tracking API host:port')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    download(args.isolate_id_list_filepath, args.tracking_api_host_port)
